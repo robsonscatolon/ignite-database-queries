025e0b367d5b919202d8c9dfdfea9bc624e20c3f
robsoncatolon <robsonscatolon@gmail.com>
2022-04-23 14:06:14 -0300

    Challenge finished

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,20 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
+    return this.repository.findOneOrFail(user_id, {
+      relations: ["games"]
+    })
     // Complete usando ORM
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM users ORDER BY first_name"); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM users WHERE first_name ilike $1 AND last_name ilike $2", [first_name, last_name]);
   }
 }
